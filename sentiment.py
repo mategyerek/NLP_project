8def94724ce50f2bf6dc36cc7493898cfb7b6297
mategyerek <materadio@gmail.com>
2025-01-09 17:03:45 +0100

    small update

--- sentiment.py
+++ sentiment.py
@@ -7,16 +7,22 @@ classifier = pipeline("sentiment-analysis",
 #cardiffnlp/twitter-roberta-base-sentiment-latest
 #distilbert-base-uncased-finetuned-sst-2-english
 #infile = "geopolitics"
-infile = "geopolitics"
+infile = "combatfootage"
+examples = True
+
+if examples:
+    limit = 10
+else:
+    limit = 1000000000
 df = pandas.read_csv(f"./data/filtered_{infile}.csv")
 
-upvotes_list = df["Upvotes"]
-text_list = df["Text"].tolist()
+upvotes_list = df["Upvotes"][0:limit]
+text_list = df["Text"].tolist()[0:limit]
 trunc_list = []
 for entry in text_list:
     trunc_list.append(str(entry)[0:800])
 # Analyze text
-result = classifier(trunc_list, batch_size=128)
+result = classifier(trunc_list, batch_size=1280)
 print(result)
 
 """
@@ -25,5 +31,6 @@ with open("./data/results.csv", "wb") as f:
 
 df_out = pandas.DataFrame(data=result)
 df_out["Upvotes"] = df["Upvotes"]
-
-df_out.to_csv(f"./data/results_{infile}.csv")
+if examples:
+    df_out["Text"] = df["Text"]
+df_out.to_csv(f"./data/examples_{infile}.csv")
