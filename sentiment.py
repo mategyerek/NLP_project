d8a99df6406ef6da89f7e0be4e5b7257aa9a4788
mategyerek <materadio@gmail.com>
2025-01-09 14:49:19 +0100

    other changes...

--- sentiment.py
+++ sentiment.py
@@ -2,12 +2,27 @@ from transformers import pipeline
 import pandas
 # Create a pipeline for sentiment analysis
 classifier = pipeline("sentiment-analysis",
-                      model="distilbert-base-uncased-finetuned-sst-2-english", device="cuda:0")
-df = pandas.read_csv("./data/geopolitics_parent_child.csv")
-text_list = df["Text"].tolist()
+                      model="distilbert-base-uncased-finetuned-sst-2-english", device=-1)
+
+limit = 100000000
+infile = "geopolitics"
+
+df = pandas.read_csv(f"./data/{infile}_parent_child.csv")
+
+upvotes_list = df["Upvotes"][0:limit]
+text_list = df["Text"].tolist()[0:limit]
 trunc_list = []
 for entry in text_list:
     trunc_list.append(entry[0:512])
 # Analyze text
 result = classifier(trunc_list)
 print(result)
+
+"""
+with open("./data/results.csv", "wb") as f:
+    """
+
+df_out = pandas.DataFrame(data=result)
+df_out["Upvotes"] = df["Upvotes"]
+
+df_out.to_csv(f"./data/results_{infile}.csv")
