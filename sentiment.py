2cd4545267b364000afdf4726193dbd7d03ecf5a
mategyerek <materadio@gmail.com>
2025-01-09 14:06:01 +0100

    intermediate commit

--- sentiment.py
+++ sentiment.py
@@ -1,9 +1,13 @@
 from transformers import pipeline
-
+import pandas
 # Create a pipeline for sentiment analysis
 classifier = pipeline("sentiment-analysis",
-                      model="distilbert-base-uncased-finetuned-sst-2-english")
-
+                      model="distilbert-base-uncased-finetuned-sst-2-english", device="cuda:0")
+df = pandas.read_csv("./data/geopolitics_parent_child.csv")
+text_list = df["Text"].tolist()
+trunc_list = []
+for entry in text_list:
+    trunc_list.append(entry[0:512])
 # Analyze text
-result = classifier("I love programming with Python!")
+result = classifier(trunc_list)
 print(result)
